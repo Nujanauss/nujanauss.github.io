c9906c18afa28a6746efda2554af9259728cf074
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-06-10 15:55:34 +0200

    remove starting instructions

--- bandit-experiment-designer/instructions2-script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('nextButton2').addEventListener('click', function() {
-    window.location.href = 'instructions3.html';
-});
--- bandit-experiment-designer/instructions3-script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('nextButton3').addEventListener('click', function() {
-    window.location.href = 'prebegin1.html';
-});
