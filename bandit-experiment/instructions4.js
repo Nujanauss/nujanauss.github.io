ea13e64114ba0e3c96502551c6a30c953b946a75
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-06-13 15:03:04 +0200

    small update

--- bandit-experiment/instructions4.js
+++ bandit-experiment/instructions4.js
@@ -1,3 +1,8 @@
-import { buttonToNewPage } from './shared.js';
+import { buttonToNewPage, getGameSettings } from './shared.js';
 
+const settings = getGameSettings();
+
+if (settings.stochastic) {
+  document.getElementById('how-much-to-score').innerHTML = 'During the training round, we will not restrict the number of moves you have. Your task is to achieve a score above 500 by clicking on the the cards.'
+}
 buttonToNewPage('trainingButton', 'training');
