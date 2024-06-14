aa26a9878d64a1da878b3a9f5d3d00fd8bea920b
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-06-14 09:40:52 +0200

    new favicon

---
+++ bandit-experiment/final.js
@@ -0,0 +1,6 @@
+import { create_participant } from './backend_integration.js';
+
+document.addEventListener('DOMContentLoaded', async function() {
+  sessionStorage.setItem('gameSettings', JSON.stringify(vars));
+  
+});
