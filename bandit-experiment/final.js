b0239f52c1230129916e6403606bd60a81f9a2b8
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-06-14 09:44:32 +0200

    send completed data

--- bandit-experiment/final.js
+++ bandit-experiment/final.js
@@ -1,6 +1,6 @@
-import { create_participant } from './backend_integration.js';
+import { send_complete } from './backend_integration.js';
 
 document.addEventListener('DOMContentLoaded', async function() {
-  sessionStorage.setItem('gameSettings', JSON.stringify(vars));
-  
+  const prolificID = JSON.parse(sessionStorage.getItem('playerData')).player.prolificID;
+  send_complete(prolificID, JSON.stringify(sessionStorage));
 });
