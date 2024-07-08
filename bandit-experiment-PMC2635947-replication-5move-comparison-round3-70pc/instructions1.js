36611f48ffe2abdae238528554a07409bb8a74b3
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-07-08 10:36:35 +0200

    improve drift

---
+++ bandit-experiment-PMC2635947-replication-5move-comparison-round3-70pc/instructions1.js
@@ -0,0 +1,33 @@
+import { buttonToNewPage, getGameSettings, initializeFocusTracker  } from './shared.js';
+import { create_participant } from './backend_integration.js';
+
+initializeFocusTracker();
+
+document.addEventListener('DOMContentLoaded', async function() {
+  const prolificID = 'prolificID';
+  create_participant(prolificID);
+
+  const vars = await loadGameSettings();
+  sessionStorage.setItem('gameSettings', JSON.stringify(vars));
+
+  async function loadGameSettings() {
+    const response = await fetch('settings.json');
+    const data = await response.json();
+    return data.vars;
+  }
+
+  const settings = getGameSettings();
+
+  var playerData = {
+    "player": {
+        "prolificID": prolificID,
+    }
+  };
+  sessionStorage.setItem('playerData', JSON.stringify(playerData));
+
+  if (settings.binary == false) {
+      document.getElementById('binary-dependent').innerHTML = 'will';
+  }
+
+  buttonToNewPage('nextButton1', 'instructions2');
+});
---
+++ bandit-experiment-PMC2635947-replication-5move-comparison-round3-70pc/instructions4.js
@@ -0,0 +1,5 @@
+import { buttonToNewPage, initializeFocusTracker } from './shared.js';
+
+initializeFocusTracker();
+buttonToNewPage('backButton4', 'instructions3');
+buttonToNewPage('trainingButton', 'training');
