1bb5d2f1524c56e55c990958702acde3a656a571
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-07-08 16:10:16 +0200

    update on data processing

--- bandit-experiment-PMC2635947-replication-control/final.js
+++ bandit-experiment-PMC2635947-replication-control/final.js
@@ -1,8 +1,13 @@
-import { send_complete, initializeFocusTracker } from './backend_integration.js';
+import { initializeFocusTracker } from './shared.js';
+import { send_complete } from './backend_integration.js';
 
 initializeFocusTracker();
 
 document.addEventListener('DOMContentLoaded', async function() {
   const prolificID = JSON.parse(sessionStorage.getItem('playerData')).player.prolificID;
-  send_complete(prolificID, JSON.stringify(sessionStorage));
+  var data = {};
+  Object.keys(sessionStorage).forEach(function(key) {
+    data[key] = JSON.parse(sessionStorage.getItem(key));
+  });
+  send_complete(prolificID, JSON.stringify(data, null, 2));
 });
