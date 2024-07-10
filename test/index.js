175d401aa70c081f1a7614cf07d749166315e4f2
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2024-07-10 09:48:34 +0200

    just use relative paths second bugfix

--- test/index.js
+++ test/index.js
@@ -3,5 +3,14 @@ import { buttonToNewPage, initializeFocusTracker } from '../bandit-experiment/sh
 initializeFocusTracker();
 
 document.addEventListener('DOMContentLoaded', async function() {
+  const vars = await loadGameSettings();
+  sessionStorage.setItem('gameSettings', JSON.stringify(vars));
+
+  async function loadGameSettings() {
+    const response = await fetch('settings.json');
+    const data = await response.json();
+    return data.vars;
+  }
+
   buttonToNewPage('first-next', '../bandit-experiment/pre-consent');
 });
